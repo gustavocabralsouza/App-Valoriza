3615ce3f00763c37977a3688795bd84b821a0c7c
Gustavo Cabral de Souza <78655626+gustavocabralsouza@users.noreply.github.com>
2022-02-05 20:03:25 -0300

    finalizacao da entidade UserActive

--- nlw6/src/entities/User.ts
+++ nlw6/src/entities/User.ts
@@ -1,6 +1,42 @@
-import {Entity} from "typeorm";
+import {
+    Entity,
+    PrimaryColumn,
+    Column,
+    CreateDateColumn, 
+    UpdateDateColumn
+} from "typeorm";
 
-@Entity()
-export class User {
+import {v4 as uuid} from 'uuid'
 
-}
+@Entity('usersActive')
+ class User {
+
+    @PrimaryColumn()
+    readonly id: string
+
+    @Column()
+    name: string
+
+    @Column()
+    email: string
+
+    @Column()
+    admin: boolean
+
+    @CreateDateColumn()
+    created_at: Date
+
+    @UpdateDateColumn()
+    updated_at: Date
+
+    constructor() {
+        //nesse if, vai ser para ver toda vez que tiver um novo usuario
+        //o simbolo !this.id diz se o id é diferente o próprio id(se ele for undefined, vazio etc)
+        if(!this.id){
+            this.id = uuid()
+        }
+    }
+
+ }
+
+export { User }
