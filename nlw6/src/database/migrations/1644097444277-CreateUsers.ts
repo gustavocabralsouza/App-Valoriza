df539e1ba37887b0d06b19d77d8269f6242098b4
Gustavo Cabral de Souza <78655626+gustavocabralsouza@users.noreply.github.com>
2022-02-05 19:40:40 -0300

    adicionando as entities

--- nlw6/src/database/migrations/1644097444277-CreateUsers.ts
+++ nlw6/src/database/migrations/1644097444277-CreateUsers.ts
@@ -1,11 +1,42 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateUsers1644097444277 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "usersActive",
+                columns:[
+                    {
+                        name: "id",
+                        type:"uuid",
+                        isPrimary: true,
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name:"email",
+                        type:"varchar"
+                    },
+                    {
+                        name:"admin",
+                        type: "boolean",
+                        default: false
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('usersActive')
     }
 
 }
