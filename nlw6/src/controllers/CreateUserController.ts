4a553378bb3e2f44653d73365c20a722f4f335b4
Gustavo Cabral de Souza <78655626+gustavocabralsouza@users.noreply.github.com>
2022-02-07 14:29:55 -0300

    #iniciando a tratativa de erros

--- nlw6/src/controllers/CreateUserController.ts
+++ nlw6/src/controllers/CreateUserController.ts
@@ -5,13 +5,18 @@ import { CreateUserService } from '../services/CreateUserService'
 class CreateUserController {
 
     async handle(request: Request, response: Response){
-        const { name, email, admin} =  request.body
+        try{
+            const { name, email, admin} =  request.body
 
-        const createUserService = new CreateUserService()
+            const createUserService = new CreateUserService()
 
-        const user = await createUserService.execute({name, email, admin})
+            const user = await createUserService.execute({name, email, admin})
 
-        return response.json(user)
+            return response.json(user)
+        }catch(err){
+            return response.status(400).json({error: err.message})
+        }
+        
     }
 }
 
