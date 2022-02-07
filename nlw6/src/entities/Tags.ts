ae668290990db2c4aa98cc107810a0f4981af6b1
Gustavo Cabral de Souza <78655626+gustavocabralsouza@users.noreply.github.com>
2022-02-07 15:20:11 -0300

    criando a camada repositorio das tags

---
+++ nlw6/src/entities/Tags.ts
@@ -0,0 +1,34 @@
+import {
+     Entity, 
+     PrimaryColumn,
+     Column,
+     CreateDateColumn,
+     UpdateDateColumn 
+} from 'typeorm'
+
+import {v4 as uuid} from 'uuid'
+
+@Entity('tagsActive')
+class Tag {
+
+    @PrimaryColumn()
+    readonly id: string
+
+    @Column()
+    name: string
+
+    @CreateDateColumn()
+    created_at : Date 
+
+    @UpdateDateColumn()
+    updated_at: Date
+
+    constructor() {
+        if(!this.id) {
+            this.id = uuid()
+        }
+    }
+
+}
+
+export {Tag}
