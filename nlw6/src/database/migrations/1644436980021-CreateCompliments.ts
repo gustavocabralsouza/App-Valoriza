9566fa9a8b3ef4c442191bb46baf7a4d2c19ab02
Gustavo Cabral de Souza <78655626+gustavocabralsouza@users.noreply.github.com>
2022-02-09 17:31:12 -0300

    foreign keys created

--- nlw6/src/database/migrations/1644436980021-CreateCompliments.ts
+++ nlw6/src/database/migrations/1644436980021-CreateCompliments.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
 
 
 export class CreateCompliments1644436980021 implements MigrationInterface {
@@ -38,12 +38,50 @@ export class CreateCompliments1644436980021 implements MigrationInterface {
                         type: "timestamp",
                         default: "now()"
                     },
+                ],foreignKeys:[
+                    {
+                        name: "FKUserSenderCompliments",
+                        referencedTableName: "usersActive",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["user_sender"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    },
+                    {
+                        name: "FKUserReceiverCompliments",
+                        referencedTableName: "usersActive",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["user_receiver"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    },
+                    {
+                        name: "FKTagCompliments",
+                        referencedTableName: "tagsActive",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["user_sender"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    },
                 ]
             })
         )
+
+        // await queryRunner.createForeignKey(
+        //     "complimentsActive",
+        //     new TableForeignKey({
+        //         name: "FKUserSenderCompliments",
+        //                 referencedTableName: "users",
+        //                 referencedColumnNames: ["id"],
+        //                 columnNames: ["user_sender"],
+        //                 onDelete: "SET NULL",
+        //                 onUpdate: "SET NULL"
+        //     })
+        // )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("complimentsActive")
     }
 
 }
