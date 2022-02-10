a5aea108233e34b5e931dcbc47d8c96a632864d4
Gustavo Cabral de Souza <78655626+gustavocabralsouza@users.noreply.github.com>
2022-02-10 16:02:06 -0300

    token authenticated

--- nlw6/src/middlewares/ensureAuthenticated.ts
+++ nlw6/src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,25 @@
 import {Request, Response, NextFunction} from 'express'
 
 
-export function ensureAdmin(request: Request, response: Response, next: NextFunction){
+export function ensureAuthenticated(
+    request: Request,
+     response: Response,
+      next: NextFunction
+){
+
+    //Receber o token
+    const token = request.headers.authorization
+   
+    //validar se o token esta preenchido
+    if(!token) {
+        return response.status(401).end()
+    }
     
+    // Valiar se token é válido
+    
+    // Recuperar informacoes do usuario
+
+    return next()
+
+
 }
