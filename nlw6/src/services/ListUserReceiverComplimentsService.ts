199e38e52f7617afa2141cf3d0cb9574d95934de
Gustavo Cabral de Souza <78655626+gustavocabralsouza@users.noreply.github.com>
2022-02-11 12:18:58 -0300

    list User Receiver Compliments

---
+++ nlw6/src/services/ListUserReceiverComplimentsService.ts
@@ -0,0 +1,22 @@
+import { getCustomRepository } from "typeorm"
+import { ComplimentsRepositories } from "../repositories/ComplimentsRepositories"
+import { UsersRepositories } from "../repositories/UsersRepositories"
+
+
+
+class ListUserReceiverComplimentsService{
+
+    async execute(user_id: string){
+        const complimentsRepositories = getCustomRepository(ComplimentsRepositories)
+
+        const compliments = await complimentsRepositories.find({
+            where: {
+              user_receiver: user_id  
+            }
+        })
+
+        return compliments
+    }
+}
+
+export {ListUserReceiverComplimentsService}
